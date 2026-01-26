558fb2b864321ebeab3a8c7ce96fda3c481dc8c0
Babkin Maksim <mak.babkin.2025@mail.ru>
2026-01-26 19:40:06 +0300

    first

---
+++ src/main/java/org/example/projectfinalspring/ProjectFinalSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.projectfinalspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFinalSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFinalSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projectfinalspring/ProjectFinalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectfinalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFinalSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
