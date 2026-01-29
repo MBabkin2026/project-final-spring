b9440cc596ffccd0aad52d60b55208a964b7657a
Babkin Maksim <mak.babkin.2025@mail.ru>
2026-01-29 20:19:53 +0300

    MOD repositories

--- src/main/java/org/example/projectfinalspring/repositories/TaskRepository.java
+++ src/main/java/org/example/projectfinalspring/repositories/TaskRepository.java
@@ -2,9 +2,11 @@ package org.example.projectfinalspring.repositories;
 
 import org.example.projectfinalspring.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByUserId(Long userId);
 }
